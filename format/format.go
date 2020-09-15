dacda6746f38b1930fd3c091f777efa619dcb2b0
Lee Choon Siong <choonsiong.lee@gmail.com>
2020-09-16 05:20:02 +0800

    Refactor

--- format/format.go
+++ format/format.go
@@ -24,9 +24,9 @@ package format
 
 import "strconv"
 
-// StringZeroToNine returns a single digit number padded with '0' in front.
+// PadZeroToNine returns a single digit number padded with '0' in front.
 // '0' => '00', '1' => '01' etc.
-func StringZeroToNine(i int) string {
+func PadZeroToNine(i int) string {
 	if i >= 0 && i < 10 {
 		return "0" + strconv.Itoa(i)
 	}
