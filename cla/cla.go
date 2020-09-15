38adc9975e81802d046d14e8d9cc87c0b1148233
Lee Choon Siong <choonsiong.lee@gmail.com>
2020-09-16 05:06:13 +0800

    Add new function

--- cla/cla.go
+++ cla/cla.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-// GetFileName returns a file name from the command line argument.
+// GetFileName returns a filename from the command line argument.
 func GetFileName() string {
 	if len(os.Args) != 2 {
 		fmt.Printf("Invalid argument, please provide a filename")
@@ -13,4 +13,16 @@ func GetFileName() string {
 	}
 
 	return os.Args[1]
+}
+
+// GetFileNames returns number of filenames specified in count.
+func GetFileNames(count int) []string {
+	total := count + 1
+
+	if len(os.Args) != total {
+		fmt.Printf("Invalid arguments, please provide %d filenames", count)
+		os.Exit(22)
+	}
+
+	return os.Args[1:]
 }
