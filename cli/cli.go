ca5942c3050d3bdc0b3dbc9afaf9d24db0f42b96
Lee Choon Siong <choonsiong.lee@gmail.com>
2022-05-20 03:30:41 +0800

    Add cli

---
+++ cli/cli.go
@@ -0,0 +1,14 @@
+package cli
+
+import (
+	"errors"
+	"os"
+)
+
+// NeedFilename returns the filename from the command-line argument.
+func NeedFilename() (string, error) {
+	if len(os.Args) != 2 {
+		return "", errors.New("please provide a filename")
+	}
+	return os.Args[1], nil
+}
