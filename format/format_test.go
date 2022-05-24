05fa1800b5ada94c43c63f1839bd49a07b2f1fd7
Lee Choon Siong <choonsiong.lee@gmail.com>
2022-05-25 01:26:24 +0800

    Update format

--- format/format_test.go
+++ format/format_test.go
@@ -26,3 +26,53 @@ func TestPadZeroToNineWithZero(t *testing.T) {
 
 	}
 }
+
+func TestLeftPaddingWithSize(t *testing.T) {
+	cases := []struct {
+		name      string
+		length    int
+		source    string
+		character string
+		want      string
+	}{
+		{"valid", 10, "hello", "*", "*****hello"},
+		{"invalid", 5, "helloworld", "*", "helloworld"},
+		{"same", 5, "hello", "*", "hello"},
+		{"negative", -10, "hello", "*", "hello"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := LeftPaddingWithSize(c.length, c.source, c.character)
+
+			if got != c.want {
+				t.Errorf("LeftPaddingWithSize(%d, %q, %q) == %q, want %q", c.length, c.source, c.character, got, c.want)
+			}
+		})
+	}
+}
+
+func TestRightPaddingWithSize(t *testing.T) {
+	cases := []struct {
+		name      string
+		length    int
+		source    string
+		character string
+		want      string
+	}{
+		{"valid", 10, "hello", "*", "hello*****"},
+		{"invalid", 5, "helloworld", "*", "helloworld"},
+		{"same", 5, "hello", "*", "hello"},
+		{"negative", -10, "hello", "*", "hello"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := RightPaddingWithSize(c.length, c.source, c.character)
+
+			if got != c.want {
+				t.Errorf("RightPaddingWithSize(%d, %q, %q) == %q, want %q", c.length, c.source, c.character, got, c.want)
+			}
+		})
+	}
+}
