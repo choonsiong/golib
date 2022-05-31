1b5837a5d8956177e4a665983f850f7b8d63e749
Lee Choon Siong <choonsiong.lee@gmail.com>
2022-05-31 17:31:15 +0800

    Add json package

---
+++ json/json.go
@@ -0,0 +1,41 @@
+package json
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+func ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1048576 // 1MB
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	d := json.NewDecoder(r.Body)
+	err := d.Decode(data)
+	if err != nil {
+		return err
+	}
+	err = d.Decode(&struct{}{})
+	if err != nil {
+		return errors.New("body must have only a single json value")
+	}
+	return nil
+}
+
+func WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
