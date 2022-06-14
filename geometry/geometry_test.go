c500afc37113a393395489438335aaa788c646b2
Lee Choon Siong <choonsiong.lee@gmail.com>
2022-06-15 00:29:09 +0800

    Auto commit

---
+++ geometry/geometry_test.go
@@ -0,0 +1,29 @@
+package geometry
+
+import (
+	"math"
+	"testing"
+)
+
+func TestSlope(t *testing.T) {
+	tests := []struct {
+		name        string
+		coordinateA CoordinateXY
+		coordinateB CoordinateXY
+		want        float64
+	}{
+		{"horizontal line", CoordinateXY{0, 0}, CoordinateXY{10, 0}, 0},
+		{"vertical line", CoordinateXY{0, 0}, CoordinateXY{0, 10}, math.Inf(1)},
+		{"positive valid", CoordinateXY{1, 2}, CoordinateXY{5, 7}, 1.25},
+		{"negative valid", CoordinateXY{1, 2}, CoordinateXY{3, -8}, -5},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Slope(tt.coordinateA, tt.coordinateB)
+			if got != tt.want {
+				t.Errorf("want %v; got %v", tt.want, got)
+			}
+		})
+	}
+}
