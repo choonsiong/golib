2bd0ba100886ee5fca30cf249ea607cd4e97b037
choonsiong <43933306+choonsiong@users.noreply.github.com>
2022-06-25 02:52:16 +0800

    Update test cases

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -2,26 +2,36 @@ package geometry
 
 import "math"
 
-// CoordinateXY represents a coordinate in two-dimensional space.
-type CoordinateXY struct {
+// Coordinate2D represents a coordinate in two-dimensional space.
+type Coordinate2D struct {
 	X float64
 	Y float64
 }
 
-// CoordinateXYZ represents a coordinate in three-dimensional space.
-type CoordinateXYZ struct {
+// Coordinate3D represents a coordinate in three-dimensional space.
+type Coordinate3D struct {
 	X float64
 	Y float64
 	Z float64
 }
 
 // Slope returns the slope of endpoints A and B.
-func Slope(a CoordinateXY, b CoordinateXY) float64 {
+func (a Coordinate2D) Slope(b Coordinate2D) float64 {
+	return Slope(a, b)
+}
+
+// Slope returns the slope of endpoints A and B.
+func Slope(a Coordinate2D, b Coordinate2D) float64 {
 	return (b.Y - a.Y) / (b.X - a.X)
 }
 
 // Midpoint returns the midpoint of endpoints A and B.
-func Midpoint(a CoordinateXY, b CoordinateXY) (float64, float64) {
+func (a Coordinate2D) Midpoint(b Coordinate2D) (float64, float64) {
+	return Midpoint(a, b)
+}
+
+// Midpoint returns the midpoint of endpoints A and B.
+func Midpoint(a Coordinate2D, b Coordinate2D) (float64, float64) {
 	x := (a.X + b.X) / 2
 	y := (a.Y + b.Y) / 2
 
@@ -29,12 +39,12 @@ func Midpoint(a CoordinateXY, b CoordinateXY) (float64, float64) {
 }
 
 // Distance3D returns the distance of endpoints A and B in three-dimensional space.
-func Distance3D(a CoordinateXYZ, b CoordinateXYZ) float64 {
+func Distance3D(a Coordinate3D, b Coordinate3D) float64 {
 	return math.Sqrt(math.Pow(a.X-b.X, 2) + math.Pow(a.Y-b.Y, 2) + math.Pow(a.Z-b.Z, 2))
 }
 
 // Midpoint3D returns the midpoint of endpoints A and B in three-dimensional space.
-func Midpoint3D(a CoordinateXYZ, b CoordinateXYZ) (float64, float64, float64) {
+func Midpoint3D(a Coordinate3D, b Coordinate3D) (float64, float64, float64) {
 	x := (a.X + b.X) / 2
 	y := (a.Y + b.Y) / 2
 	z := (a.Z + b.Z) / 2
@@ -43,7 +53,7 @@ func Midpoint3D(a CoordinateXYZ, b CoordinateXYZ) (float64, float64, float64) {
 }
 
 // CoordinateForMidPoint3D returns the endpoint B with given endpoint A and midpoint M.
-func CoordinateForMidPoint3D(a CoordinateXYZ, m CoordinateXYZ) (float64, float64, float64) {
+func CoordinateForMidPoint3D(a Coordinate3D, m Coordinate3D) (float64, float64, float64) {
 	x := (m.X * 2) - a.X
 	y := (m.Y * 2) - a.Y
 	z := (m.Z * 2) - a.Z
--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -8,14 +8,14 @@ import (
 func TestSlope(t *testing.T) {
 	tests := []struct {
 		name        string
-		coordinateA CoordinateXY
-		coordinateB CoordinateXY
+		coordinateA Coordinate2D
+		coordinateB Coordinate2D
 		want        float64
 	}{
-		{"horizontal line", CoordinateXY{0, 0}, CoordinateXY{10, 0}, 0},
-		{"vertical line", CoordinateXY{0, 0}, CoordinateXY{0, 10}, math.Inf(1)},
-		{"positive valid", CoordinateXY{1, 2}, CoordinateXY{5, 7}, 1.25},
-		{"negative valid", CoordinateXY{1, 2}, CoordinateXY{3, -8}, -5},
+		{"horizontal line", Coordinate2D{0, 0}, Coordinate2D{10, 0}, 0},
+		{"vertical line", Coordinate2D{0, 0}, Coordinate2D{0, 10}, math.Inf(1)},
+		{"positive slope", Coordinate2D{0, 0}, Coordinate2D{10, 10}, 1},
+		{"negative slope", Coordinate2D{0, 0}, Coordinate2D{-10, 10}, -1},
 	}
 
 	for _, tt := range tests {
@@ -26,25 +26,47 @@ func TestSlope(t *testing.T) {
 			}
 		})
 	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.coordinateA.Slope(tt.coordinateB)
+			if got != tt.want {
+				t.Errorf("want %v; got %v", tt.want, got)
+			}
+		})
+	}
 }
 
 func TestMidpoint(t *testing.T) {
 	tests := []struct {
 		name        string
-		coordinateA CoordinateXY
-		coordinateB CoordinateXY
+		coordinateA Coordinate2D
+		coordinateB Coordinate2D
 		midpointX   float64
 		midpointY   float64
 	}{
-		{"positive valid", CoordinateXY{0, 0}, CoordinateXY{10, 10}, 5, 5},
-		{"negative valid", CoordinateXY{0, 0}, CoordinateXY{-10, -10}, -5, -5},
-		{"vertical line", CoordinateXY{0, 10}, CoordinateXY{0, 0}, 0, 5},
-		{"horizontal line", CoordinateXY{0, 0}, CoordinateXY{10, 0}, 5, 0},
+		{"positive coordinates", Coordinate2D{0, 0}, Coordinate2D{10, 10}, 5, 5},
+		{"negative coordinates", Coordinate2D{0, 0}, Coordinate2D{-10, -10}, -5, -5},
+		{"vertical line", Coordinate2D{0, 10}, Coordinate2D{0, 0}, 0, 5},
+		{"horizontal line", Coordinate2D{0, 0}, Coordinate2D{10, 0}, 5, 0},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			wantX, wantY := Midpoint(tt.coordinateA, tt.coordinateB)
+			if tt.midpointX != wantX {
+				t.Errorf("want midpoint x %v; got %v", tt.midpointX, wantX)
+			}
+
+			if tt.midpointY != wantY {
+				t.Errorf("want midpoint y %v; got %v", tt.midpointY, wantY)
+			}
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			wantX, wantY := tt.coordinateA.Midpoint(tt.coordinateB)
 			if tt.midpointX != wantX {
 				t.Errorf("want midpoint x %v; got %v", tt.midpointX, wantX)
 			}
