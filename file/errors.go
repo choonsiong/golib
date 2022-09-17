2d98ae94e3e2e3cf3ea70506778e259490b5f2c0
choonsiong <43933306+choonsiong@users.noreply.github.com>
2022-09-17 22:47:55 +0800

    Add directory

--- file/errors.go
+++ file/errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrCloseFile       = errors.New("file: file close error")
+	ErrCreateDir       = errors.New("file: failed to create directory")
 	ErrFileNotFound    = errors.New("file: file not found")
 	ErrInvalidFilename = errors.New("file: invalid filename")
 	ErrInvalidTriplet  = errors.New("file: invalid triplet")
