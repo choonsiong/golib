c07e4404732e48b09c4d37f3dd574d1ba071fb4e
Lee Choon Siong <choonsiong.lee@gmail.com>
2022-10-09 23:50:08 +0800

    Add maths

---
+++ maths/floats.go
@@ -0,0 +1,12 @@
+package maths
+
+import "math"
+
+// CompareFloats compares f1 and f2 using the given epsilon.
+func CompareFloats(f1 float64, f2 float64, e float64) bool {
+	if math.Abs(f1-f2) > e {
+		return false
+	}
+
+	return true
+}
