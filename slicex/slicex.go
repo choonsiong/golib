abaf19cdca35b43c40c61a1529f0dabe7161e1c8
Lee Choon Siong <choonsiong.lee@gmail.com>
2022-10-22 17:57:50 +0800

    Add slicex

---
+++ slicex/slicex.go
@@ -0,0 +1,44 @@
+package slicex
+
+import (
+	"reflect"
+)
+
+// Contains returns true if element e is found in slice s else returns false.
+// The type T can be either int, string or bool only.
+func Contains[T any](e T, s []T) bool {
+	r1 := reflect.ValueOf(&e).Elem()
+
+	for _, v := range s {
+		r2 := reflect.ValueOf(&v).Elem()
+		switch r2.Type().Name() {
+		case "int":
+			if r1.Int() == r2.Int() {
+				return true
+			}
+		case "string":
+			if r1.String() == r2.String() {
+				return true
+			}
+		case "bool":
+			if r1.Bool() == r2.Bool() {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// DeleteElementAtIndex returns a new slice with element at index deleted.
+func DeleteElementAtIndex[T any](i int, s []T) []T {
+	if len(s) == 0 {
+		return s
+	}
+
+	if i > (len(s) - 1) {
+		return s
+	}
+
+	return append(s[:i], s[i+1:]...)
+}
